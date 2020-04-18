a53bc0b4777ad65a491c6e557ef570d5b216c1f5
SunFlower <1835550631@qq.com>
2020-04-18 21:07:22 +0800

    第一次提交

---
+++ .svn/pristine/f4/f4b996b0c13a23f173ad1906940245106618e8a5.svn-base
@@ -0,0 +1,8 @@
+package svnproject;
+
+public class TestBranch {
+	public static void main(String[] args) {
+		System.out.println("¿ªÆô·ÖÖ§");
+	}
+
+}
---
+++ src/svnproject/Test.java
@@ -0,0 +1,12 @@
+package svnproject;
+
+public class Test {
+public static void main(String[] args) {
+	System.out.println("aaa");
+	System.out.println("update again");
+	System.out.println("解决冲突");
+	System.out.println("合并冲突");
+	System.out.println("再次发生冲突");
+	System.out.println("开发分支代码");
+}
+}
